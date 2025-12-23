7c6add29ef75a9a904b9751d12ed94e0f855290b
Dev Jeff <arribion@gmail.com>
2025-12-23 18:33:46 +0300

    pre save password on schema before saving on database

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -38,6 +39,22 @@ const userSchema = new mongoose.Schema({
 },{
     timestamps: true
 });
+
+// pre save password before saving to database
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next()
+  } catch (error) {
+    next(error);
+  }
+});
+
+userSchema.methods.comparePassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+}
 
 export const userModel = mongoose.model("UserCollection", userSchema);
 
